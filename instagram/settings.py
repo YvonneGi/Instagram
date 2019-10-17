1fdabbb8b75f43e2093cfde698bea0003c2ea64e
YvonneGi <giryvonne@gmail.com>
2019-10-17 14:51:50 +0200

    create navbar and welcome message

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'photogram.apps.PhotogramConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- photogram/views.py
+++ photogram/views.py
@@ -1,3 +1,19 @@
+from django.http  import HttpResponse
 from django.shortcuts import render
+# import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        searched_users = Profile.search_profile(search_term)
+        message=f"Search results for: {search_term}"
+
+        return render(request,'search.html',{"message":message,"users":searched_users})
+
+    else:
+        message="You haven't searched for any term."
+        return render(request,'search.html',{"message":message})
