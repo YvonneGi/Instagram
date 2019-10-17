40d3b4e7577659e765e39a667ef814f25f4b1aa8
YvonneGi <giryvonne@gmail.com>
2019-10-17 10:14:28 +0200

    create photogram app

---
+++ photogram/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PhotogramConfig(AppConfig):
+    name = 'photogram'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wecode/Documents/Python-projects/Instagram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
