05f54e67e2b9e66265ce582e6a47148506cd967a
YvonneGi <giryvonne@gmail.com>
2019-10-20 19:47:48 +0200

    update views function

--- photogram/urls.py
+++ photogram/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     # url(r'^search/', views.search_user, name='search_user'),
-    # url(r'^explore/', views.explore, name = 'explore'),
-    # url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
+    url(r'^explore/', views.explore, name = 'explore'),
+    url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
     # url(r'^new/post/', views.new_post, name = 'new-post'),
     # url(r'^accounts/edit-profile/', views.edit_profile, name = 'edit-profile'),
     # url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico'))
--- photogram/views.py
+++ photogram/views.py
@@ -70,3 +70,74 @@ def welcome(request):
 
     return render(request,'welcome.html',{"posts":posts,"profiles":profiles,"current_user":current_user,"comments":comments,"form":form, "likeform":likeform, "likes":likes,"likez":likez,})
 
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        searched_users = Profile.search_profile(search_term)
+        message=f"Search results for: {search_term}"
+
+        return render(request,'search.html',{"message":message,"users":searched_users})
+
+
+
+@login_required(login_url='/accounts/login/')
+def explore(request):
+    posts = Post.objects.all()
+    profiles= Profile.objects.all()[:3]
+    
+    return render(request,"explore.html",{"posts":posts,"profiles":profiles,})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,id):
+    user_object = request.user
+    current_user = Profile.objects.get(username__id=request.user.id)
+    user = Profile.objects.get(username__id=id)
+    posts = Post.objects.filter(upload_by = user)
+    follows = Follow.objects.all()
+
+    if request.method == 'POST' and 'follower' in request.POST:
+        print("follow saved")
+        followed_user_id = request.POST.get("follower")
+        followform = FollowForm(request.POST)
+        if followform.is_valid():
+            followed_user_id = int(request.POST.get("follower"))
+            current_user = Profile.objects.get(username__id=request.user.id)
+            follow = followform.save(commit=False)
+            follow.username = request.user
+            followed_user = User.objects.get(pk=followed_user_id)
+            print(followed_user)
+            follow.followed = followed_user
+            follow.follow_id = str(follow.username.id)+"-"+str(follow.followed.id)
+            follow.save()
+            print("follow saved")
+
+        return redirect("profile", user.username.id)
+    else:
+        followform = FollowForm()
+
+    if request.method == 'POST' and 'unfollower' in request.POST:
+        followed_user_id = request.POST.get("unfollower")
+        followed_user = User.objects.get(pk=followed_user_id)
+        follow_id = str(request.user.id)+"-"+str(followed_user.id)
+        follow_delete = Follow.objects.get(follow_id=follow_id)
+        follow_delete.delete()
+
+
+
+    follows = Follow.objects.all()
+    followz = Follow.objects.values_list('follow_id', flat=True)
+    followz =list(followz)
+    follower =0
+    following = 0
+    for follow in followz:
+        follow = follow.split("-")
+        if follow[0] == str(user.username.id):
+            following+=1
+        if follow[-1] == str(user.username.id):
+            follower+=1
+
+
+    return render(request, "profile.html", {"current_user":current_user,"posts":posts,"user":user,"user_object":user_object, "follows":follows, "followz":followz,"follower":follower,"following":following})
+
+
